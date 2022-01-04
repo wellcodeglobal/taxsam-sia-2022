3e52f91c39671b72c0bdda624a33a11bf8a0a5a0
didirdt <didi.rudiatd12@gmail.com>
2022-01-04 13:21:18 +0700

    fix precompile error webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.merge(customConfig);
 module.exports = environment
