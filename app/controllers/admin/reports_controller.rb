76f5714315a4027bc3d62fca8f5943e02d21697d
didirdt <didi.rudiatd12@gmail.com>
2022-06-21 14:21:29 +0700

    add destroy fore import report

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -6,4 +6,13 @@ class Admin::ReportsController < AdminController
   def show    
     @report_facede = Admin::Reports::IndexFacade.new(params)
   end
+
+  def destroy
+    @report = Report.find_by_id(params[:id])
+    if @report.present? && @report.destroy
+      return redirect_to admin_reports_path, notice: "Laporan Berhasil di hapus."
+    end
+
+    redirect_to admin_reports_path, alert: "Laporan tidak ditemukan."
+  end
 end
